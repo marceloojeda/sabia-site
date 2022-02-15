0371211bdf812d42753b6df810d69b9ea0946205
Marcelo Ojeda <marcelo@upgestao.com.br>
2022-02-15 19:17:44 -0300

    Corrigindo layout das tela de cadastro e alteracao de membro.

--- resources/views/coordenador/team/edit.blade.php
+++ resources/views/coordenador/team/edit.blade.php
@@ -5,26 +5,29 @@
 
 @section('content')
 
-<form action="/teams/{{$user->id}}" method="post">
-    @csrf
-    @method('put')
-    <div class="card">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-sm-6 form-group">
-                    <label for="name">Nome</label>
-                    <input class="form-control" type="text" name="name" id="name" value="{{$user->name}}">
-                </div>
-                <div class="col-sm-4 form-group">
-                    <label for="phone">Telefone</label>
-                    <input class="form-control" type="text" name="phone" id="phone" onkeypress="mask(this, mphone);" onblur="mask(this, mphone);" value="{{$user->phone}}">
+<div class="col-sm-12">
+    <form action="/teams/{{$user->id}}" method="post">
+        @csrf
+        @method('put')
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-6 form-group">
+                        <label for="name">Nome</label>
+                        <input class="form-control" type="text" name="name" id="name" value="{{$user->name}}">
+                    </div>
+                    <div class="col-sm-4 form-group">
+                        <label for="phone">Telefone</label>
+                        <input class="form-control" type="text" name="phone" id="phone" onkeypress="mask(this, mphone);" onblur="mask(this, mphone);" value="{{$user->phone}}">
+                    </div>
                 </div>
             </div>
+            <div class="card-footer">
+                <button type="button" class="btn btn-danger" onclick="sellerDelete({{ $user->id }})">Excluir da Equipe</button>
+                <button type="submit" class="btn btn-primary float-right">Salvar</button>
+            </div>
         </div>
-        <div class="card-footer">
-            <button type="button" class="btn btn-danger" onclick="sellerDelete({{ $user->id }})">Excluir da Equipe</button>
-            <button type="submit" class="btn btn-primary float-right">Salvar</button>
-        </div>
-    </div>
-</form>
+    </form>
+</div>
+
 @endsection
